cfeebd2d727d9d63ab3ad5492e28a4de29cfc840
pvar9 <pvarshney123@gmail.com>
2018-03-04 18:46:41 -0800

    testing gui git

---
+++ tree.py
@@ -0,0 +1,36 @@
+def printLevelByLevel(self, root):
+	currLevel = [root]
+
+	while(currLevel):
+		newLevel = []
+		for n in currLevel:
+			print n.value
+		if root.left != None:
+			newLevel.append(root.left)
+		if root.right != None:
+			newLevel.append(root.right)
+		currLevel = newLevel
+
+def averageOfLevels(self, root):
+    """
+    :type root: TreeNode
+    :rtype: List[float]
+    """
+    currLevel = [root]
+    result = []
+    
+    while currLevel: 
+        newLevel = []
+        len_level = len(currLevel)
+        temp = []
+        for n in currLevel:
+            temp.append(n.val)
+        sum_level = sum(temp)
+        result.append(sum_level/len_level)
+        if root.left != None:
+            newLevel.append(root.left)
+        if root.left != None: 
+            newLevel.append(root.right)
+        currLevel = newLevel
+    return result
+
